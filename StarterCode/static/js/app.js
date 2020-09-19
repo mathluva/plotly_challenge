9dd8dd3d123306bebe6d3af352322392ecf7a125
mathluva <mathluvacoding@gmail.com>
2020-09-19 00:14:18 -0400

    prepare bar data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,14 +1,42 @@
-d3.json("samples.json").then(function(data){
-    console.log(data);
-    var trace1 = {
-        x: data.samples,
-        y: data.names,
-        marker: {
-          color: 'rgba(1,1,1,0.0)'
-        },
-        type: 'bar'
-      };
-    Plotly.newPlot("bar",[trace1]);
-});
+var x = [];
+var y = [];
 
-   
+//use d3 to read json file
+d3.json("samples.json").then( data => { 
+  
+  //map values to a variable called map
+  var map = d3.map(data); 
+       
+  // Calling the map.values() function
+  // Getting an array of values for 
+  // every entry in the map. 
+
+  A = map.values(); 
+  //check values in console window
+  
+  //console.log(A);
+  
+  // grab only information from samples
+  var bar_data = A[2]
+  // ???Slice the first 10 objects for plotting ???
+  bar_data = bar_data.slice(0,10);
+  //check data
+  //console.log(bar_data);
+     for (var i=0; i< bar_data.length; i++)
+     {
+       //push sample_values and otu_ids
+       //to the empty list x and y
+        x.push(bar_data[i].sample_values);
+        y.push(bar_data[i].otu_ids);
+      };
+       
+});
+console.log(x);
+console.log(y);
+trace = {
+  x: x,
+  y: y,
+  type:'bar',
+  orientation: 'h' };
+
+Plotly.newPlot("bar",[trace]);
